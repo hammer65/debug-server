4335e8f7a1c0f015e2f27a303889d259c54bca7f
hammer65 <hammer65@gmail.com>
2015-04-10 15:55:21 -0500

    list code

--- app/app.js
+++ app/app.js
@@ -1,1 +1,13 @@
 var nwgui = require('nw.gui');
+
+var lf = require('logfile')W;
+var win = lf.Window.get();
+var dom = win.window;
+
+lf.list(null,{logDir:'../../log'},function(e,data){
+	var el = dom.$('#groups');
+	for(var i = 0;i < data.length;i++){
+		var lnk = dom.$('<a href="#" id="cat_' + data[i] + '">' + data[i] + '</a>')
+		el.append();
+	}
+});
