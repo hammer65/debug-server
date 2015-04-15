e8bc26dea596b0e27503dcada43f19e6e3c44314
Chris <hammer65@gmail.com>
2015-04-15 15:30:00 -0500

    further mods and rework

--- app/app.js
+++ app/app.js
@@ -1,30 +1,10 @@
 var nwgui = require('nw.gui');
-
+var app = require('application');
 var lf = require('logfile');
 
 var win = nwgui.Window.get();
 var dom = win.window;
-var logconfig = {logDir:'../../log'};
+var logconfig = {logs:'../../log'};
+var application = new app(logconfig);
 
-function listMain(){
-	lf.list(null,logconfig,function(e,data){
-		if(e){ console.log(e); return false}
-		var el = dom.$('#groups');
-		var row, lnk;
-		for(var i = 0;i < data.length;i++){
-			row = dom.$('<div class="category"></div>');
-			lnk = dom.$('<a href="#" data-category="' + data[i] + '" id="cat_' + data[i] + '">' + data[i] + '</a><br />');
-			lnk.click(function(event){
-				var el = event.target;
-				logWindow(el.getAttribute('data-category'));
-				event.preventDefault();
-			});
-			row.append(lnk);
-			el.append(row);
-		}
-	});
-}
-
-
-
-listMain();
+//application.list();
